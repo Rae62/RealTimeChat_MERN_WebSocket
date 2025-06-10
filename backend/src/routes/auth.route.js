f494d9e15a4d41837e35e169dc88356990053019
Rae62 <ludovic.duhamel-62260@outlook.fr>
2025-06-10 13:56:21 +0200

    Routes dones

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,10 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
+
+router.post("/sign-up", signup);
+router.post("/sign-in", login);
+router.post("/logout", logout);
 
 export default router;
