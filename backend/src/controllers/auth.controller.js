6edf10fdd3e52052df03d72cce3e5254eefcbfa1
Rae62 <ludovic.duhamel-62260@outlook.fr>
2025-06-10 14:43:47 +0200

    signin and up done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,74 @@
-export const signup = (req, res) => {};
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
-export const login = (req, res) => {};
+export const signup = async (req, res) => {
+  const { fullname, email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({
+        message: "Email already used",
+      });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullname,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullname: newUser.fullname,
+        email: newUser.email,
+        avatar: newUser.avatar,
+      });
+    } else {
+      return res.status(400).json({
+        message: "User data invalid",
+      });
+    }
+  } catch (error) {
+    console.log("error in signup controller", error);
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Données utilisateur invalides",
+      });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({
+        message: "Données utilisateur invalides",
+      });
+    }
+    generateToken(user._id, res);
+    res.status(201).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profileAvatar: user.profileAvatar,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error);
+  }
+};
 
 export const logout = (req, res) => {};
