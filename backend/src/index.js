7a33f76f40266b40ca7bec094858a311da6e3e2a
Rae62 <ludovic.duhamel-62260@outlook.fr>
2025-06-11 14:35:50 +0200

    signUp Logout done

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,20 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 const PORT = process.env.PORT;
 
